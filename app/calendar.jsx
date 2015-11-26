4e0969d610a9746a25cae24f1e4acfd2b905bbfb
buzz1274 <david@sulaco.co.uk>
2015-11-26 00:32:18 +0000

    calendar mockup

--- app/calendar.jsx
+++ app/calendar.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
+var days = [];
+
+for(var i = 1; i <= 35; i++) {
+  days[i] = {'key': i,
+             'date': i};
+}
+
 export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.viewDay = this.viewDay.bind(this);
+
+  }
   render() {
     return (<div className="col-xs-8 holder">
-              <div id="calendar">
-                Calendar
+              <div id="calendar" onClick={this.viewDay}>
+                <div className="header">
+                  <div className="title">
+                    <span title="Previous Month" className="pull-left">
+                      ««
+                    </span>
+                    November 2015
+                    <span title="Next Month" className="pull-right">
+                      »»
+                    </span>
+                  </div>
+                </div>
+                {days.map(this.renderDay)}
               </div>
             </div>
     );
+  }
+  renderDay(day) {
+    return (<div className="col-xs-1 calendar_day">
+              <div className="inner">
+                {day.date}
+              </div>
+            </div>
+    );
+  }
+  viewDay() {
+    alert("HERE");
   }
 }
--- app/styles/bootstrap.config.js
+++ app/styles/bootstrap.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  styleLoader: require('extract-text-webpack-plugin').extract('style-loader', 'css-loader!postcss-loader!less-loader'),
+  styleLoader: require('extract-text-webpack-plugin').extract('style-loader',
+                                                              'css-loader!postcss-loader!less-loader'),
   scripts: {
     'transition': true,
-  },  
+  },
   styles: {
   	'normalize': true,
   	'tables': true,
   	'mixins': true,
+    'navs': true,
   	"scaffolding": true,
     "grid": true,
   }
